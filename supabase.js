f8faa915127fd072887fad959c908f74a40a6e9c
Belich2025 <belich.avisos@gmail.com>
2026-05-09 20:32:10 +0200

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,9 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { createClient } from '@supabase/supabase-js'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const SUPABASE_URL = 'https://sqwbxmewymvmnegszzte.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxd2J4bWV3eW12bW5lZ3N6enRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyODMzNzcsImV4cCI6MjA5Mzg1OTM3N30.z_vGOPEqZXTQp9hWaiAjU-7Q1s8vACwfseB4UWIrfgM'
+
+export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
